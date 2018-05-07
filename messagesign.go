bd7934d993b1cd61127e334c741e11951a9e001e
Wilson Qin <wilson.qin@gmail.com>
2018-05-07 16:24:54 -0400

    make GenerateKeyPair public, remove flag parsing from this package

--- messagesign.go
+++ messagesign.go
@@ -10,7 +10,7 @@ import (
 	crand "crypto/rand"
 	"encoding/hex"
 	"encoding/json"
-	"flag"
+	// "flag"
 	//"fmt"
 	"io/ioutil"
 
@@ -20,31 +20,31 @@ import (
 )
 
 // Flags
-var gFlag = flag.String("set-generate", "", "Generates to a new keypair; g|set-generate [filename]")
-var vtFlag = flag.String("verify-text", "", "Verify a text message; v|verify-text [filename]")
-var vbFlag = flag.String("verify-bin", "", "Verify a binary message; b|verify-bin [filename]")
-var stFlag = flag.String("sign-text", "", "Sign a binary message; s|verify-bin [filename]")
-var sbFlag = flag.String("sign-bin", "", "Sign a text message; z|verify-bin [filename]")
-var krFlag = flag.String("keyring", "", "Load a keyring of pubkeys; k|keyring [filename]")
-var kpFlag = flag.String("keypair", "", "Load a keypair to sign from; p|keypair [filename]")
-var sigFlag = flag.String("sig", "", "Load a Base58 signature to verify; S|sig [filename]")
-var blindFlag = flag.Bool("blind", false, "Enable signature blinding (non-unique!); B|blind")
+// var gFlag = flag.String("set-generate", "", "Generates to a new keypair; g|set-generate [filename]")
+// var vtFlag = flag.String("verify-text", "", "Verify a text message; v|verify-text [filename]")
+// var vbFlag = flag.String("verify-bin", "", "Verify a binary message; b|verify-bin [filename]")
+// var stFlag = flag.String("sign-text", "", "Sign a binary message; s|verify-bin [filename]")
+// var sbFlag = flag.String("sign-bin", "", "Sign a text message; z|verify-bin [filename]")
+// var krFlag = flag.String("keyring", "", "Load a keyring of pubkeys; k|keyring [filename]")
+// var kpFlag = flag.String("keypair", "", "Load a keypair to sign from; p|keypair [filename]")
+// var sigFlag = flag.String("sig", "", "Load a Base58 signature to verify; S|sig [filename]")
+// var blindFlag = flag.Bool("blind", false, "Enable signature blinding (non-unique!); B|blind")
 
 func init() {
 	// Short version flags
-	flag.StringVar(gFlag, "g", "", "Generates to a new keypair; g|set-generate [filename]")
-	flag.StringVar(vtFlag, "v", "", "Verify a text message; v|verify-text [filename]")
-	flag.StringVar(vbFlag, "b", "", "Verify a binary message; b|verify-bin [filename]")
-	flag.StringVar(stFlag, "s", "", "Sign a text message; s|verify-bin [filename]")
-	flag.StringVar(sbFlag, "z", "", "Sign a binary message; z|verify-bin [filename]")
-	flag.StringVar(krFlag, "k", "", "Load a keyring of pubkeys; k|keyring [filename]")
-	flag.StringVar(kpFlag, "p", "", "Load a keypair to sign from; p|keypair [filename]")
-	flag.StringVar(sigFlag, "S", "", "Load a Base58 signature to verify; S|sig [filename]")
-	flag.BoolVar(blindFlag, "B", false, "Enable signature blinding (non-unique!); B|blind")
+	// flag.StringVar(gFlag, "g", "", "Generates to a new keypair; g|set-generate [filename]")
+	// flag.StringVar(vtFlag, "v", "", "Verify a text message; v|verify-text [filename]")
+	// flag.StringVar(vbFlag, "b", "", "Verify a binary message; b|verify-bin [filename]")
+	// flag.StringVar(stFlag, "s", "", "Sign a text message; s|verify-bin [filename]")
+	// flag.StringVar(sbFlag, "z", "", "Sign a binary message; z|verify-bin [filename]")
+	// flag.StringVar(krFlag, "k", "", "Load a keyring of pubkeys; k|keyring [filename]")
+	// flag.StringVar(kpFlag, "p", "", "Load a keypair to sign from; p|keypair [filename]")
+	// flag.StringVar(sigFlag, "S", "", "Load a Base58 signature to verify; S|sig [filename]")
+	// flag.BoolVar(blindFlag, "B", false, "Enable signature blinding (non-unique!); B|blind")
 }
 
 // generateKeyPair generates and stores an ECDSA keypair to a file.
-func generateKeyPair(filename string) error {
+func GenerateKeyPair(filename string) error {
 	// Generate keypairs.
 	aKeypair, err := ecdsa.GenerateKey(btcec.S256(), crand.Reader)
 	if err != nil {
